d534a7b6e62cf351c29aa72a487a4f7049431d45
Pilar <Pilar@10.42.102.19>
2013-07-27 14:28:30 -0500

    PRUEBA

---
+++ src/conexiondb/ConexionDB.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexiondb;
+import java.sql.*;
+/**
+ *
+ * @author Pilar
+ */
+public class ConexionDB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)  {
+        // TODO code application logic here
+        
+       //FrmAlumnoAlta frmAltaAlumno=new FrmAlumnoAlta();
+       //frmAltaAlumno.setVisible(true);
+        FrmAlumnoPrincipal alumnop=new FrmAlumnoPrincipal();
+        alumnop.setVisible(true);
+    }
+}
